49aa04b962a561cf190104a27ea265e31e4d4d7e
imzhuli <imzhuli@vip.qq.com>
2025-08-08 15:04:32 +0800

    upgrade device challenge protocol

--- cpp/src_lib_protocol/pp_protocol/cc_dv/device.hpp
+++ cpp/src_lib_protocol/pp_protocol/cc_dv/device.hpp
@@ -3,26 +3,39 @@
 
 struct xCC_DeviceChallenge : xBinaryMessage {
     void SerializeMembers() override {
-        W(AppVersion, Timestamp, Sign);
+        W(AppVersion, ChannelId, Timestamp);
+        W(PrimaryIpv4Address);
+        W(PrimaryIpv6Address);
+        W(Ipv4UdpEnabled);
+        W(Ipv6UdpEnabled);
+        W(Sign);
     };
 
     void DeserializeMembers() override {
-        R(AppVersion, Timestamp, Sign);
+        R(AppVersion, ChannelId, Timestamp);
+        R(PrimaryIpv4Address);
+        R(PrimaryIpv6Address);
+        R(Ipv4UdpEnabled);
+        R(Ipv6UdpEnabled);
+        R(Sign);
     };
 
-    uint32_t    AppVersion = 0;
-    uint64_t    Timestamp  = 0;
+    uint32_t AppVersion = 0;
+    uint32_t ChannelId  = 0;
+    uint64_t Timestamp  = 0;
+
+    xNetAddress PrimaryIpv4Address;
+    xNetAddress PrimaryIpv6Address;
+    bool        Ipv4UdpEnabled = false;
+    bool        Ipv6UdpEnabled = false;
+
     std::string Sign;
 };
 
 struct xCC_DeviceChallengeResp : xBinaryMessage {
-    void SerializeMembers() override {
-        W(CtrlAddress, DataAddress, CheckKey, TerminalAddress, UseOldVersion, EnableSpeedControl);
-    };
+    void SerializeMembers() override { W(CtrlAddress, DataAddress, CheckKey, TerminalAddress, UseOldVersion, EnableSpeedControl); };
 
-    void DeserializeMembers() override {
-        R(CtrlAddress, DataAddress, CheckKey, TerminalAddress, UseOldVersion, EnableSpeedControl);
-    };
+    void DeserializeMembers() override { R(CtrlAddress, DataAddress, CheckKey, TerminalAddress, UseOldVersion, EnableSpeedControl); };
 
     xNetAddress CtrlAddress;
     xNetAddress DataAddress;
