07935c2cef0691c6d532795e7ba86e1473a7a2f6
imzhuli <imzhuli@vip.qq.com>
2025-08-20 10:50:15 +0800

    remove xGroupedMessage

--- cpp/src_lib_protocol/pp_protocol/base.hpp
+++ cpp/src_lib_protocol/pp_protocol/base.hpp
@@ -1,11 +1,2 @@
 #pragma once
 #include <pp_common/_.hpp>
-
-class xGroupedMessage : public xBinaryMessage {
-public:
-    void SerializeMembers() override { W(GroupId); }
-    void DeserializeMembers() override { R(GroupId); }
-
-public:
-    xGroupId GroupId = 0;
-};
