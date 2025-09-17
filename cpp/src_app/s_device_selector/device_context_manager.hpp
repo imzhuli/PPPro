6ae2be9f9872166985b48cd8bff8e12a9f3d7085
imzhuli <imzhuli@vip.qq.com>
2025-09-18 06:38:33 +0800

    DEVICE_INFO_ENABLE_COUNTER_TARGET

--- cpp/src_app/s_device_selector/device_context_manager.hpp
+++ cpp/src_app/s_device_selector/device_context_manager.hpp
@@ -2,8 +2,9 @@
 #include <pp_common/_.hpp>
 #include <unordered_map>
 
-static constexpr const uint64_t DEVICE_KEEPALIVE_TIMEOUT_MS = 180'000;  // normally device is removed by request from dispatcher
-static constexpr const size_t   DEVICE_INFO_RESIST_COUNTER  = 2;        // normally device is removed by request from dispatcher
+static constexpr const uint64_t DEVICE_KEEPALIVE_TIMEOUT_MS       = 180'000;  // normally device is removed by request from dispatcher
+static constexpr const size_t   DEVICE_INFO_RESIST_COUNTER        = 2;        // normally device is removed by request from dispatcher
+static constexpr const size_t   DEVICE_INFO_ENABLE_COUNTER_TARGET = 3;
 
 struct xDR_TimeoutNode : xListNode {
     uint64_t TimestampMS;
@@ -29,6 +30,7 @@ struct xDS_DeviceContext
     , xDR_CityNode {
     xDR_DeviceInfoBase InfoBase;
     size_t             ResisterCounter = DEVICE_INFO_RESIST_COUNTER;
+    size_t             EnableCounter   = 0;
 };
 
 class xDS_DeviceContextManager {
